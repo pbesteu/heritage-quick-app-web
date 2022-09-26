66b002c029917c54eeb3f58808844a65cf7971bf
Martin Alvarez <martin@espinr.es>
2022-09-26 15:42:35 +0200

    Details and maps

--- public/js/init.js
+++ public/js/init.js
@@ -1,8 +1,62 @@
+
+const myPosition = { lat: 0, lon: 0};
+
 (function($){
   $(function(){
     $('.sidenav').sidenav();
-    $(document).ready(function(){
-      $('.modal').modal();
-    });    
+    $('.modal').modal({dismissible:false});
+    $('.tabs').tabs();
+
+    if ('geolocation' in navigator) {
+      $('#modalinit').modal('open');
+      navigator.geolocation.getCurrentPosition((position) => {
+        $('#modalinit').modal('close');
+        // console.log(position.coords.latitude + " " + position.coords.longitude);
+        myPosition.lat = position.coords.latitude;
+        myPosition.lon = position.coords.longitude
+        updateDistancePoIs();
+      });
+    } else {
+      /* geolocation IS NOT available */
+      console.error('geolocation IS NOT available')
+    }
+
+    // Opens the dialog by default
+    if (modalToOpenAtInit) {
+      $(`#modal_${modalToOpenAtInit}`).modal('open');
+    }
+    
   }); // end of document ready
+
+  /**
+   * Converts the meters into a more friendly string (in metres or kms)
+   * @param {integer} value with the distance in metres 
+   * @returns a string with the value in human-readable format (metres, or kms)
+   */
+  function prettifyDistance(value) { 
+    try {
+      const number = parseInt(value);
+      if (number > 1000) {
+        return `${(value/1000).toFixed(2)} Kms.`;        
+      }
+    } catch (ex) {
+      return ''
+    }
+    return `${value} m.`;        
+  }
+
+  function updateDistancePoIs() {
+    $('.card-distance').each((index, obj) => {
+      const distance = window.geolib.getDistance(
+        { latitude: myPosition.lat, longitude: myPosition.lon },
+        { 
+          latitude: obj.getAttribute('data-lat'),
+          longitude: obj.getAttribute('data-lon') 
+        }
+      );
+      $(obj).text(`${prettifyDistance(distance)} from your location`);
+    });
+  }
+
 })(jQuery); // end of jQuery name space
+
--- routes/town.js
+++ routes/town.js
@@ -4,18 +4,23 @@ module.exports = async function (fastify, opts) {
 
   const fetch = require('cross-fetch');
 
-  fastify.get('/town/:url', async function (request, reply) {
+  fastify.get('/town/:url/:identifier', async function (request, reply) {
     const { url } = request.params;
-    // fetches the resource
+    let { identifier } = request.params;
+    // fetches the resource from the URL
+
     try {
       const res = await fetch(url);
-      console.log(res)
       if (res.status >= 400) {
         throw new Error("Bad response from server");
       }
       const json = await res.json();
       const firstKeyInContent = Object.keys(json.content)[0];
-      return reply.view("/templates/index.ejs", { meta: json.meta, content: json.content[firstKeyInContent] });
+
+      return reply.view("/templates/index.ejs", { 
+        meta: json.meta, 
+        content: json.content[firstKeyInContent], 
+        identifier });
     } catch (err) {
       return reply.view("/templates/error.ejs", { message: err.toString() });
     }
