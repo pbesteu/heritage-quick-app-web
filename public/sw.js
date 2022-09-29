160484f6685954e248db62bbcec079243bed41b3
Martin Alvarez <martin@espinr.es>
2022-09-29 17:08:59 +0200

    New service worker

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+
+
+const cacheName = 'appcache';
+const cachedFiles = [
+  '/public/images/background.jpg',
+  '/public/images/background.jpg',
+  '/public/images/placeholder.jpg'
+];
+
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching content');
+    await cache.addAll(cachedFiles);
+  })());
+});
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }));
+  }));
+});
